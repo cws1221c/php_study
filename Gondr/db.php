2611a4ce8733cbb25b94bdc785b71907385d5093
cws1221c <woos2911@naver.com>
2019-05-26 14:55:23 +0900

    php공부

---
+++ Gondr/db.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Gondr;
+
+class DB {
+    private static $db = null;
+
+    public static function getDB(){
+        if(self::$db == null){
+            self::$db = new \PDO(
+                "mysql:host=gondr.asuscomm.com;dbname=yy_30217;charset=utf8mb4;",
+                "yy_30217", "1234");
+        }
+        return self::$db;
+    }
+
+    public static function fetchAll($sql, $data = []){
+        $q = self::getDB()->prepare($sql);
+        $q->execute($data);
+        return $q->fetchAll(\PDO::FETCH_OBJ);
+    }
+}
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+use Gondr\Route;
+
+require ("vendor/autoload.php");
+require ("web.php");
+
+define("__ROOT", __DIR__);
+
+$url = isset($_GET['url']) ? "/" . $_GET['url'] : "/";
+
+Route::route($url);
